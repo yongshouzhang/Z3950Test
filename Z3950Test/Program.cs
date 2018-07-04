023024647aa198d9573728aca08429ea971dc297
Dell <e60321@163.com>
2018-07-05 01:25:00 +0800

    z3950 控制台项目

--- Z3950Test/Program.cs
+++ Z3950Test/Program.cs
@@ -2,13 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using Z3950;
 namespace Z3950Test
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var server = new Server()
+            {
+                Name = "mytest",
+                //Uri = "z3950.loc.gov",
+                Uri = "202.96.31.28",
+                Port = 9991,
+                DatabaseName = "UCS01U",
+                Username = "L410581YHH",
+                Password = "20141030"
+            };
+            int count = 0;
+            var tt = server.GetRecords(Bib1Attr.Title, "javascript", 10, 1, out count);
+            tt.ToList().ForEach(obj =>
+            {
+                Console.WriteLine(obj);
+            });
+
+            Console.ReadKey();
         }
     }
 }
